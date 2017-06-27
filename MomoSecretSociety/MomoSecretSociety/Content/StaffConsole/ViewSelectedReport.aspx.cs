99e50f6b04a9c6615546d9197bae4126ddc1f6bd
Mandeal <Mandeal@DESKTOP-EFJTFKS>
2017-06-27 09:45:54 +0800

    Redirect from GridView

--- MomoSecretSociety/MomoSecretSociety/Content/StaffConsole/ViewSelectedReport.aspx.cs
+++ MomoSecretSociety/MomoSecretSociety/Content/StaffConsole/ViewSelectedReport.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,35 @@ namespace MomoSecretSociety.Content.StaffConsole
 {
     public partial class ViewSelectedReport : System.Web.UI.Page
     {
+        static SqlConnection viewSelectedReportsDetailsConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FileDatabaseConnectionString2"].ConnectionString);
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!IsPostBack)
+            {
+                string abc = "";
+
+                viewSelectedReportsDetailsConnection.Open();
+                SqlCommand selectedDetailsCommand = new SqlCommand("SELECT * FROM Report WHERE CaseNumber = @CaseNumber", viewSelectedReportsDetailsConnection);
+                selectedDetailsCommand.Parameters.AddWithValue("@CaseNumber", Session["caseNumberOfThisSelectedReport"].ToString());
+
+
+                SqlDataReader selectedReportsDetailsReader = selectedDetailsCommand.ExecuteReader();
+
+                while (selectedReportsDetailsReader.Read())
+                {
+                    //let say i reetrieve username
+                    abc = selectedReportsDetailsReader["Username"].ToString();
+                }
+                viewSelectedReportsDetailsConnection.Close();
+
+                Response.Write(abc);
+            }
         }
+
+
+
     }
 }
