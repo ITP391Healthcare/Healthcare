4ddaf3860d02be8b470156aa26b76c2f5fd74282
ASUS <ASUS@KAYIONG>
2017-07-11 10:40:06 +0800

    test redirect url validate

---
+++ MomoSecretSociety/MomoSecretSociety/Content/StaffConsole/testRedirectStaff.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MomoSecretSociety.Content.StaffConsole
+{
+    public partial class testRedirectStaff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (System.Web.HttpContext.Current.Request.Url.ToString() == "http://localhost:53380/Content/BossConsole/PendingReports")
+            {
+                if (Context.User.Identity.Name != "KaiTatL97")
+                {
+
+                    Page.ClientScript.RegisterStartupScript(GetType(), "alert", "cannot go boss la ", true);
+                    
+                }
+            }
+        }
+    }
+}
